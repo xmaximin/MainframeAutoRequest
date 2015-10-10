51fc1dbc8425cf8ce0d36f25987b3e5ce2ce29b1
xmaximin <xmaximin@gmail.com>
2015-10-10 20:51:32 +0200

    preparation internet layer

--- app/src/main/java/com/codeattitude/mainframeautorequest/controllers/LoginActivity.java
+++ app/src/main/java/com/codeattitude/mainframeautorequest/controllers/LoginActivity.java
@@ -1,21 +1,21 @@
 package com.codeattitude.mainframeautorequest.controllers;
 
-import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.ImageView;
 
 import com.codeattitude.mainframeautorequest.R;
 
 public class LoginActivity extends AppCompatActivity {
 
-    // current ressources
-    EditText username;
-    EditText password;
-    Button login;
+    // current resources
+    EditText editTestUsername;
+    EditText editTextPassword;
+    Button btnLogin;
 
 
     @Override
@@ -27,39 +27,46 @@ public class LoginActivity extends AppCompatActivity {
         setResources();
         setColorAndFont();
 
-        // attach event on button
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Send the authenticate request
+                String username = editTestUsername.getText().toString();
+                String password = editTextPassword.getText().toString();
 
 
+                // if success :
+                // store token on preference android (crypted if possible)
+                // then go to RequestsActivity
+
+                //if error :
+                // display error message red with Toast
+            }
+        });
 
     }
-
-
-
-
-
 
 
     // private methods
-    private void setResources(){
-        username = (EditText)findViewById(R.id.editTextUsername);
-        password = (EditText)findViewById(R.id.editTextPassword);
-        login = (Button) findViewById(R.id.login);
+    private void setResources() {
+        editTestUsername = (EditText) findViewById(R.id.editTextUsername);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        btnLogin = (Button) findViewById(R.id.login);
     }
 
-    private void setColorAndFont(){
+    private void setColorAndFont() {
         Typeface tf = getTypeFaceFont("fonts/Museo Sans/MuseoSans-300.otf");
-        username.setTypeface(tf, Typeface.BOLD);
-        password.setTypeface(tf, Typeface.BOLD);
-        login.setTypeface(tf, Typeface.BOLD);
+        editTestUsername.setTypeface(tf, Typeface.BOLD);
+        editTextPassword.setTypeface(tf, Typeface.BOLD);
+        btnLogin.setTypeface(tf, Typeface.BOLD);
 
     }
 
-    private Typeface getTypeFaceFont(String fontfile){
+    private Typeface getTypeFaceFont(String fontfile) {
         Typeface tf = Typeface.createFromAsset(getAssets(),
-               fontfile);
+                fontfile);
         return tf;
     }
-
 
 
 }
---
+++ app/src/main/java/com/codeattitude/mainframeautorequest/model/User.java
@@ -0,0 +1,7 @@
+package com.codeattitude.mainframeautorequest.model;
+
+/**
+ * Created by xaviermaximin on 10/10/2015.
+ */
+public class User {
+}
